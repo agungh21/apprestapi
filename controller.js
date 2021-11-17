6256268c372f313127967c5ae625a6028b56bd3b
agungh21 <agungmuhammad70@gmail.com>
2021-11-17 17:46:29 +0700

    menampilkan semua data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("App Rest Api Sedang berjalan", res);
 };
+
+// menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if (error) {
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+}
--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ module.exports = function(app){
 
     app.route('/')
         .get(myjson.index);
+
+    app.route('/mhs')
+        .get(myjson.tampilSemuaMahasiswa);
 }
