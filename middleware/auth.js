66fd04c5f0829345a2eed9149f822ec3602277d1
agungh21 <agungmuhammad70@gmail.com>
2021-11-19 07:25:06 +0700

    + auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,46 @@
+var connection = require('../koneksi');
+var mysql = require('mysql');
+var md5 = require('md5');
+var response = require('../res');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+var ip = require('ip');
+
+// controller untuk daftar
+exports.daftar = function(req, res){
+    var post = {
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+        tanggal_daftar: req.body.tanggal_daftar
+        
+    };
+
+    var query = "SELECT email FROM ?? WHERE ??";
+    var table = ["user", "email", post.email];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else{
+            if(rows.length == 0){
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query, table);
+
+                connection.query(query, post, function (error, rows) {
+                    if(error){
+                        console.log(error);
+                    }else{
+                        response.ok("Berhasil Menambahkan user");
+                    } 
+                });
+            }else{
+                response.ok("Email Sudah Terdaftar");
+            }
+        }
+    });
+};
